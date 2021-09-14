25055c3fb43d12f9d8dccdfc2d0cda92da13e55f
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-09-15 03:00:43 +0500

    create and style carousel for cars with static data and make it responsive

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,3 +1,4 @@
 export { BookCard } from './HomePageSections/BookCard';
 export { BookingSteps } from './HomePageSections/BookCard';
 export { AboutUs } from './HomePageSections/About.section';
+export { TopCars } from './HomePageSections/TopCars.section';
